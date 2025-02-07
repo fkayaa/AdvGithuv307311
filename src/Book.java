2f82738a89d156ee143ced92bcdd5ed4225ce0cd
Fatma Kaya <fatmackaya888@gmail.com>
2025-02-07 20:47:20 +0300

    v2

---
+++ src/Book.java
@@ -0,0 +1,6 @@
+public class Book {
+
+    //değişiklik2
+    public String name;
+
+}
--- src/MyClass.java
+++ src/MyClass.java
@@ -3,6 +3,9 @@ public class MyClass {
     public static void main(String[] args) {
         //değişiklik 1
         System.out.println("Slm");
+
+        //degişiklik 2
+        System.out.println("Adv Github...");
     }
 
 }
